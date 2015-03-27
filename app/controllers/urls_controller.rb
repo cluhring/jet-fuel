987e57ab9b931796c403b7130a8aeee5d77a2280
Chris Luhring <cluhring@gmail.com>
2015-03-27 12:37:52 -0600

    original url validations w/ passing tests

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,4 +1,14 @@
 class UrlsController < ApplicationController
   def index
   end
+
+  def create
+    @url = URL.new(url_params)
+  end
+
+  private
+
+  def url_params
+    params.require(url).permit(:original_url)
+  end
 end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UrlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "url is valid" do
+    valid_url = Url.create(original_url: "https://github.com/")
+    assert valid_url.valid?
+  end
+
+  test "url is invalid if not present" do
+    invalid_url = Url.create(original_url: "")
+    assert invalid_url.invalid?
+  end
+
+  test "url is invalid with an invalid url" do
+    invalid_url = Url.create(original_url: "ghp://yahoo.com")
+    assert invalid_url.invalid?
+  end
+
 end
