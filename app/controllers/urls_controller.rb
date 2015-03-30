4193d7343dccb7bb22000a5fd6ec7e77a0cacfcc
Chris Luhring <cluhring@gmail.com>
2015-03-29 19:35:43 -0600

    heroku fix

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -8,11 +8,11 @@ class UrlsController < ApplicationController
   end
 
   def show
-    if params[:tiny_url]
+    # if params[:tiny_url]
       @url = Url.find_by(tiny_url: (params[:tiny_url]))
-    else
-      @url = Url.find(params[:id])
-    end
+    # else
+    #   @url = Url.find(params[:id])
+    # end
     @url.clicks += 1
     @url.save
     redirect_to @url.original_url
