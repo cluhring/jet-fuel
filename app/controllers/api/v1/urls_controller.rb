0ca8c6e0e7bb7049fa424256fc2ffd63d3726f2f
Chris Luhring <cluhring@gmail.com>
2015-03-29 23:02:47 -0600

    interal api created and tested

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -10,4 +10,26 @@ class Api::V1::UrlsController < ApplicationController
     @url = Url.find(params[:id])
     respond_with @url
   end
+
+  def create
+    @url = Url.create(url_params)
+    respond_with @url
+  end
+
+  def update
+    @url = Url.update(params[:id], url_params)
+    respond_with @url
+  end
+
+  def destroy
+    @url = Url.destroy(params[:id])
+    respond_with @url
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:original_url, :tiny_url, :clicks)
+  end
+
 end
--- test/controllers/api/v1/urls_controller_test.rb
+++ test/controllers/api/v1/urls_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class Api::V1::UrlsControllerTest < ActionController::TestCase
+
   test '#index' do
     url1 = Url.create(original_url: "https://www.github.com/1287", tiny_url: '2', clicks: 5)
     url2 = Url.create(original_url: "https://www.github.com/1286", tiny_url: '3', clicks: 3)
@@ -27,5 +28,41 @@ class Api::V1::UrlsControllerTest < ActionController::TestCase
     assert_equal 'https://www.github.com/1287', url['original_url']
     assert_equal '2', url['tiny_url']
     assert_equal 5, url['clicks']
+  end
+
+  test '#create' do
+    assert_difference('Url.count', 1) do
+      post :create, format: :json, url: {original_url: "https://www.github.com/1247", tiny_url: '88', clicks: 5}
+    end
+
+    url = JSON.parse(response.body)
+
+    assert_response 201 #created
+    assert_equal 'https://www.github.com/1247', url['original_url']
+    assert_equal '88', url['tiny_url']
+    assert_equal 5, url['clicks']
+  end
+
+  test '#update' do
+    url1 = Url.create(original_url: "https://www.github.com/1287", tiny_url: '2', clicks: 5, id: 2)
+
+    patch :update, format: :json, id: 2, url: { original_url: "https://www.github.com/1234", tiny_url: '2', clicks: 55 }
+    url = Url.find(2)
+    assert_response 204 #no response
+    assert_equal 'https://www.github.com/1234', url['original_url']
+    assert_equal '2', url['tiny_url']
+    assert_equal 55, url['clicks']
+  end
+
+  test '#destroy' do
+    url1 = Url.create(original_url: "https://www.github.com/1287", tiny_url: '2', clicks: 5, id: 2)
+    assert_difference('Url.count', -1) do
+      delete :destroy, format: :json, id: 2
+    end
+    assert_response 204 #no response
+    assert_response :success
+    assert_raises(ActiveRecord::RecordNotFound) do
+      Url.find(2)
+    end
   end
 end
